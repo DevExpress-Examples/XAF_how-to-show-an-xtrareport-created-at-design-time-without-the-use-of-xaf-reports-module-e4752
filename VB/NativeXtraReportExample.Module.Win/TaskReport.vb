05cc19906ac6a948294724c5f9aeb7e08b17af37
DevExpressExampleBot <fakeEmail>
2019-01-29 17:51:07 +0300

    Source auto update [skip ci]

---
+++ VB/NativeXtraReportExample.Module.Win/TaskReport.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports DevExpress.XtraReports.UI
+
+Namespace NativeXtraReportExample.Module.Win
+    Partial Public Class TaskReport
+        Inherits DevExpress.XtraReports.UI.XtraReport
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+End Namespace
---
+++ VB/NativeXtraReportExample.Module/BusinessObjects/Task.bo.vb
@@ -0,0 +1,8 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace NativeXtraReportExample.Module.BusinessObjects
+    Partial Public Class Task
+    End Class
+End Namespace
